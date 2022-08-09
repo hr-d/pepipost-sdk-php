ad54b0a2d29067e3d626595536a6836af38a65c8
hrdf <h.r.daneshvar.69@gmail.com>
2022-08-09 12:54:16 +0430

    update api base url

--- src/Configuration.php
+++ src/Configuration.php
@@ -17,7 +17,7 @@ class Configuration
      * The base Uri for API calls
      * @var string
      */
-    public static $BASEURI = 'https://emailapi.netcorecloud.net/v5/mail';
+    public static $BASEURI = 'https://emailapi.netcorecloud.net/v5';
 
     /**
      * Your Pepipost API Key. You will find the api key in the Pepipost application in Integrations.
